76ff11f5605f6071eb03bfc7fd8a24446fe62e54
Arjun Nemani <nemani@fb.com>
2021-07-06 17:10:00 +0100

    init example repo

---
+++ docs/source/index.rst
@@ -0,0 +1,7 @@
+Welcome to Getting Started with Sphinx's documentation!
+=======================================================
+
+.. automodule:: myproject.main
+   	:members:
+   	:undoc-members:
+   	:show-inheritance:
---
+++ myproject/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import math
+
+def method(param_one, param_two=2, param_three=3):
+    """
+    Compute bootstrapped standard error of mean of input data.
+
+    :param param_one: ONE
+    :param param_two: TWO
+    :param param_three: THREE
+    """
+    return param_one + param_three + param_two
